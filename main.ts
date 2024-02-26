a2a84822bc5acf8f47d382c49daef6c2eecf6f96
chino-cochino-9 <156123454+chino-cochino-9@users.noreply.github.com>
2024-02-26 13:07:35 -0600

    put in enemies, made a health bar for player trying to figure how to destroy enemy and slash

--- main.ts
+++ main.ts
@@ -3,25 +3,24 @@ namespace SpriteKind {
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     slash = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . 8 . . . . . 
-        . . . . . . . . . 1 8 . . . . . 
-        . . . . . . . . 1 8 . . . . . . 
-        . . . . . . . . . 8 . . . . . . 
-        . . . . . . 8 8 8 . . . . . . . 
-        . . . . . 8 8 . . . . . . . . . 
-        . . . . . 8 . 1 . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
+        . . . . . . . . 6 9 6 . . . . . 
+        . . . . . . . 6 9 9 6 . . . . . 
+        . . . . . . . 6 9 6 6 . . . . . 
+        . . . . . . . 6 9 6 . . . . . . 
+        . . . . . . . 6 9 6 . . . . . . 
+        . . . . . . . 6 9 6 . . . . . . 
+        . . . . . . . 6 9 6 . . . . . . 
+        . . . . . . . 6 9 6 . . . . . . 
+        . . . . . . . 6 9 6 . . . . . . 
+        . . . . . . . 6 9 6 . . . . . . 
+        . . . . . . . 6 9 6 . . . . . . 
+        . . . . . . . 6 9 6 . . . . . . 
+        . . . . . . . 6 9 6 . . . . . . 
+        . . . . . . . 6 9 6 . . . . . . 
+        . . . . . . . 6 9 9 6 . . . . . 
+        . . . . . . . 6 6 9 6 . . . . . 
         `, you, Render.getAttribute(Render.attribute.dirX) * 100, Render.getAttribute(Render.attribute.dirY) * 100)
-    sprites.destroy(slash, effects.spray, 500)
-    slash.setFlag(SpriteFlag.AutoDestroy, true)
+    sprites.destroy(slash, effects.coolRadial, 500)
 })
 function startup () {
     tiles.setCurrentTilemap(tilemap`level`)
@@ -154,8 +153,28 @@ function startup () {
     scaling.scaleByPixels(sword, 20, ScaleDirection.Horizontally, ScaleAnchor.Middle)
     scaling.scaleByPixels(sword, 20, ScaleDirection.Vertically, ScaleAnchor.Middle)
 }
+statusbars.onZero(StatusBarKind.Health, function (status) {
+    game.splash("YOU DIE", "TRY AGAIN?")
+    game.reset()
+})
+function spawnen (enlist: Image[]) {
+    encount = 0
+    for (let index = 0; index < 5; index++) {
+        enemies = sprites.create(enlist._pickRandom(), SpriteKind.Enemy)
+        tiles.placeOnRandomTile(enemies, sprites.castle.tilePath5)
+        enemies.follow(you, 20)
+        encount += 1
+    }
+}
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    playbar.value += -5
+    pause(500)
+})
+let enemies: Sprite = null
+let encount = 0
 let sword: Sprite = null
 let slash: Sprite = null
+let playbar: StatusBarSprite = null
 let you: Sprite = null
 you = Render.getRenderSpriteVariable()
 startup()
@@ -187,57 +206,62 @@ img`
     ........................
     `,
 img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `,
-img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . f f f . . . . . . . . . . . 
+    f f f c c . . . . . . . . f f f 
+    f f c c . . c c . . . f c b b c 
+    f f c 3 c c 3 c c f f b b b c . 
+    f f b 3 b c 3 b c f b b c c c . 
+    . c b b b b b b c f b c b c c . 
+    . c b b b b b b c b b c b b c . 
+    c b 1 b b b 1 b b b c c c b c . 
+    c b b b b b b b b c c c c c . . 
+    f b c b b b c b b b b f c . . . 
+    f b 1 f f f 1 b b b b f c c . . 
+    . f b b b b b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `,
 img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . f f f f f f f f f . . . 
+    `,
+img`
+    . . . . c c c c c c . . . . . . 
+    . . . c 6 7 7 7 7 6 c . . . . . 
+    . . c 7 7 7 7 7 7 7 7 c . . . . 
+    . c 6 7 7 7 7 7 7 7 7 6 c . . . 
+    . c 7 c 6 6 6 6 c 7 7 7 c . . . 
+    . f 7 6 f 6 6 f 6 7 7 7 f . . . 
+    . f 7 7 7 7 7 7 7 7 7 7 f . . . 
+    . . f 7 7 7 7 6 c 7 7 6 f c . . 
+    . . . f c c c c 7 7 6 f 7 7 c . 
+    . . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+    . c 7 7 2 7 7 c f c 6 7 7 6 c c 
+    c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+    f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+    f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+    . f 6 1 1 1 1 1 1 6 6 6 f . . . 
+    . . c c c c c c c c c f . . . . 
     `
 ]
+spawnen(enlist)
+playbar = statusbars.create(100, 15, StatusBarKind.Health)
+playbar.value = 100
+playbar.setFlag(SpriteFlag.RelativeToCamera, true)
+playbar.setPosition(80, 110)
